fc3d5f7cbccbf309dd5cd64652f99b850c67b0c1
JohnHito <johnjairo988@gmail.com>
2025-09-28 23:46:19 -0600

    Debug

--- config/cors.php
+++ config/cors.php
@@ -1,35 +1,12 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Aquí defines la configuración de CORS.
-    | Ajustado para permitir que el frontend en Vite (5173) acceda al backend.
-    |
-    */
-
-    // Solo aplicamos CORS en las rutas de la API
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => [
-        'http://localhost:5173', // tu entorno local de React/Vite
-       /*>>>>>>>>>>>>>>>> */ 'https://TUDOMINIO.FRONTEND.com', // tu futuro dominio de producción (opcional) //RECORDAR CAMBIAR<<<<<<<<<<<<<<<<<<<<<<<<
-        // Puedes agregar otros orígenes aquí
-    ],
-
+    'allowed_origins' => ['*'], // <--- ABIERTO A TODOS
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => false,
 ];
