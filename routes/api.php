a5b61152935acda23eaa6542231df892e6dc6f3a
JohnHito <johnjairo988@gmail.com>
2025-10-02 22:10:36 -0600

    se actualizo el api.php parcialmente

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 
+// Controllers
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\NewPasswordController;
@@ -11,7 +12,15 @@ use App\Http\Controllers\VendorController;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ProductImageController; // <-- FALTABA ESTE IMPORT
+use App\Http\Controllers\ProductImageController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ContactMessageController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+// NOT FOUND CONTROLLERS
+// use App\Http\Controllers\StoreController;
+// use App\Http\Controllers\StoreBannerController;
+// use App\Http\Controllers\StoreReviewController;
+// use App\Http\Controllers\StoreSocialController;
 
 /*
 |--------------------------------------------------------------------------
@@ -40,9 +49,7 @@ Route::get('/products/search', [ProductController::class, 'search']);      // bu
 Route::get('/products/{id}', [ProductController::class, 'show']);          // detalle
 Route::get('/products/vendor/{vendorId}', [ProductController::class, 'byVendor']); // productos por vendor
 
-
-
-// Prueba DB
+// DB Test
 Route::get('/db-test', function () {
     try {
         $result = DB::select('select current_database() as db, inet_server_addr() as host');
@@ -63,7 +70,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/users', [UserController::class, 'listUsers']);
     Route::get('/me', [UserController::class, 'me']);
     Route::post('/logout', [UserController::class, 'logout']);
-    Route::put('/clients/{id}', [ClientController::class, 'update']);
+    //Route::put('/clients/{id}', [ClientController::class, 'update']);
     Route::get('/users/{id}', [UserController::class, 'show']);
     Route::put('/change-password', [UserController::class, 'changePassword']);
     Route::delete('/users/{id}', [UserController::class, 'destroy']);
@@ -72,8 +79,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/profiles/{id}', [ProfileController::class, 'show']);
 
     // Vendors (CRUD + productos de un vendor)
-    Route::apiResource('vendors', VendorController::class);
-    Route::get('/vendors/{id}/products', [VendorController::class, 'products']);
+    //Route::apiResource('vendors', VendorController::class);
+    //Route::get('/vendors/{id}/products', [VendorController::class, 'products']);
 
     // Productos (CRUD completo)
     Route::post('/products', [ProductController::class, 'store']);
@@ -81,7 +88,17 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 
     // Imágenes de producto 
-    Route::get('/products/{id}/images', [ProductImageController::class, 'index']);        // listar imágenes de un producto
-    Route::post('/product-images', [ProductImageController::class, 'store']);             // subir nueva imagen
-    Route::delete('/product-images/{id}', [ProductImageController::class, 'destroy']);    // eliminar imagen
+    //Route::get('/products/{id}/images', [ProductImageController::class, 'index']);        
+    //Route::post('/product-images', [ProductImageController::class, 'store']);             
+    //Route::delete('/product-images/{id}', [ProductImageController::class, 'destroy']);   
+    
+    // Ordenes (CRUD)
+    Route::apiResource('orders', OrderController::class);
+
+    // Mensajes de contacto (CRUD)
+    Route::apiResource('contact-messages', ContactMessageController::class);
+
+    // Sesiones autenticadas
+    Route::post('/session', [AuthenticatedSessionController::class, 'store']);
+    Route::delete('/session', [AuthenticatedSessionController::class, 'destroy']);
 });
