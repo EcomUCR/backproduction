d697e7a205dee6f4b09ea04324017074ec70b1fd
JohnHito <johnjairo988@gmail.com>
2025-10-18 17:19:49 -0600

    a

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Stripe\Stripe;
 use Stripe\PaymentIntent;
+use App\Models\Cart;
 
 class PaymentController extends Controller
 {
