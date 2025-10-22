880958895c96f442dd2ed0081b4456930daf359a
JohnHito <johnjairo988@gmail.com>
2025-10-22 17:57:03 -0600

    Correo de tienda

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -119,7 +119,7 @@ class UserController extends Controller
 
                 if ($admins->isNotEmpty()) {
                     $subject = 'Nueva solicitud de verificación de tienda';
-                    $body = view('emails.store_request', [
+                    $body = view('emails.verification_request', [
                         'store_name' => $store->name,
                         'owner_name' => trim($user->first_name . ' ' . $user->last_name) ?: $user->username,
                         'owner_email' => $user->email,
